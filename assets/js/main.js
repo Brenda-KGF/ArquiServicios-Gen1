55d345b8c383831a75cd90075289d73c070665b3
BrendaGomez <iq.bren12@gmail.com>
2025-12-14 22:17:12 -0600

    Parte 3: Diseño del menú, header, footer y navegación completa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
 console.log("Proyecto Web iniciado correctamente.");
+
+function toggleMenu() {
+    const nav = document.getElementById("navLinks");
+    nav.classList.toggle("nav-open");
+}
+
+console.log("Script cargado correctamente.");
